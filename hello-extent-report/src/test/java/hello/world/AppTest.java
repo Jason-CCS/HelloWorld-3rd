913ca115e9218fe3e2d8e2c6d1f5672314d59843
jasonchang.ccs <jasonchang.ccs@gmail.com>
2019-08-06 17:30:02 +0800

    commit

--- hello-extent-report/src/test/java/hello/world/AppTest.java
+++ hello-extent-report/src/test/java/hello/world/AppTest.java
@@ -1,20 +1,43 @@
 package hello.world;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+import static org.testng.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> currentThread = new ThreadLocal<>();
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
+
+    @BeforeClass
+    public synchronized void beforeClass() {
+        ExtentTest parent = extent.createTest(getClass().getName());
+        currentThread.set(parent);
+    }
+
+    @BeforeMethod
+    public synchronized void beforeMethod(Method method) {
+        ExtentTest child = currentThread.get().createNode(method.getName());
+
+
+    }
+
+
 }
